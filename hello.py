338a2d4691a468063d0f44cba17a5fac236ca6b2
Parautilitarian <102628338+Parautilitarian@users.noreply.github.com>
2025-09-02 23:03:30 +0000

    add a hello.py and test

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ def add(a: int, b: int) -> int:
     """Return the sum of two numbers."""
     return a + b
 
+
 if __name__ == "__main__":
     print(say_hello("Prof. Yu"))
     print("2 + 3 =", add(2, 3))
